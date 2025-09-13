057f717202a645887c7504e79e8cc2178fc94ea8
Henrique Hackbart Neto <henriquehackcode@gmail.com>
2025-09-12 23:09:43 -0300

    db-Postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 link_db = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///comunidade.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "a3190c71717b80582c2b580d8bc02528"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
